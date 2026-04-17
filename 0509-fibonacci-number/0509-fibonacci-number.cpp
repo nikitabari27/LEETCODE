49ffd0324d6841cc5f51988f1079b43ecf77a04b
Nikita Bari <nikitabari27@gmail.com>
2026-04-17 12:21:19 +0530

    Time: 3 ms (64.34%), Space: 8.4 MB (10.87%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
 
-int topDownApproach(vector<int> &dp , int n){
-    //Base case
-    if(n==1 ||n==0){
-        return n;
+int bottomUp(int n){
+   //step1 -> create dp
+   vector<int>dp(n+1, -1);
+
+    //step2-< observe base case
+    dp[0] =0;
+
+    if(n==0){
+        return dp[0];
     }
+    dp[1] =1;
 
-   // step3 -> check if ans already exist
-     if(dp[n] != -1){    // kyuki apan ne initialize -1 se kiya tha agr -1 nhi mtlb ans h
-        return dp[n];
-     }
-
-    // step2-> store ans in dp
-     dp[n]= topDownApproach(dp ,n-1) + topDownApproach(dp ,n-2);
+    //step3
+    for(int i=2; i<=n; i++){
+        dp[i] =dp[i-1] +dp[i-2];
+    }
     return dp[n];
 }
     int fib(int n) {
-        
-        //step1->Create dp
-        vector<int>dp(n+1 ,-1);
 
-       return  topDownApproach (dp ,n);
-        
+
+         return bottomUp(n);
     }
 };
