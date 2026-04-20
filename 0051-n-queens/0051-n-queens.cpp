2d9cf0b95b29d948f01d5c0ca49d4b60109f8ad7
Nikita Bari <nikitabari27@gmail.com>
2026-04-20 23:40:34 +0530

    Time: 6 ms (19.63%), Space: 11.1 MB (25.66%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,109 +1,92 @@
 class Solution {
 public:
-void storeSolution(vector<vector<char>> &board, int n, vector<vector<string>> &ans){
+
+void storeSoln(vector<vector<char>> &board,  vector<vector<string>> &ans, int n ){
 
     vector<string> temp;
-     for(int i=0; i<n; i++){
+
+    for(int row=0; row<n; row++){
+
         string output;
-          for(int j=0; j<n; j++){ 
-       output.push_back(board[i][j]);
-      }
-         temp.push_back(output);
-     }
 
-         ans.push_back(temp);
+        for(int col=0; col<n; col++){
+
+
+                output.push_back(board[row][col]);
+            
+         }
+           temp.push_back(output);
+    }
+    ans.push_back(temp);
+}
+
+bool isSafe( vector<vector<char>> &board, int row, int col ,int n){
+
+    int r=row;
+    int c=col;
+    // left row
+    while(c >= 0){
+
+        if(board[r][c]=='Q'){
+            return false;
+        }
+        c--;
+ }
+ // upper left diagonal
+     r= row;
+    c =col;
+    while(r>=0 && c>=0){
+
+        if(board[r][c]=='Q'){
+            return false;
+        }
+        r--;
+        c--;
    }
+   //Bottom left diagonal
+    r= row;
+    c= col;
+   while(r < n && c >=0){
 
-bool isSafe(int row, int col, vector<vector<char>> &board , int n){
-
-   //check krna h ki current cell[row, col] m queen rkh skta hu yha nhi
-
-   int i= row; 
-   int j= col;
-
-   // hme sirf piche ki 3 direction check krni h 
-
-   //kyuki  ek col m ek hi queen place ho skti hi isliye selected col kr upar aur niche
-   // check krne ki zarurat nhi h
-
-   // Aur aage wale col m bhi check nhi krege
-
-   //1 direction -> check left row 
-   while(j >= 0){
-      if(board[i][j] == 'Q'){
-         return false;
-      }
-      j--;
+        if(board[r][c]=='Q'){
+            return false;
+        }
+        r++;
+        c--;
    }
-
-   // 2-> upper left diagonal
-   i = row;
-   j =col;
-   while(i>=0 && j>=0){
-      if(board[i][j] == 'Q'){
-         return false;
-      }
-      i--;
-      j--;
-   }
-
-   //3 -> bottom left diagonal
-    i = row;
-   j =col;
-   while(i<n && j>=0){
-      if(board[i][j] == 'Q'){
-         return false;
-      }
-      i++;
-      j--;
-   }
-
-   // 3 ho direction m kahi bhi queen nhi mili
-   // mtlb position safe h to queen ko place kr do
-
    return true;
 }
 
+void solve( vector<vector<char>> &board , int col, int n, vector<vector<string>> &ans){
 
-void solve(vector<vector<char>> &board , int col, int n,  vector<vector<string>> &ans){
+    //Base case
+    if(col >=n){
+        storeSoln(board, ans, n);
+        return ;
+    }
 
-   //Base case
-   if(col >= n){
-      // N queens are placed in Nn col
-      storeSolution(board, n, ans);
-      return;
-   }
+    for(int row=0; row<n; row++){
+        if(isSafe(board, row, col, n)){
 
-   // 1 case solve kro baki rec dekh lega
-
-   //1 case bole to 0th col  ke har row ke andar queen place krna ka try krna h
-    
-   for(int row=0; row<n; row++){
-      if(isSafe(row , col, board , n )){
-         // Agr safe h to queen ko rkh do
-         board[row][col]='Q';
-
-         //Recursion dekh lega
-         solve(board , col+1, n ,ans );
-
-         //Backtracking
-          board[row][col]='.';
-      }
-   }
-}
-    vector<vector<string>> solveNQueens(int n){
-   
-   vector<vector<char>> board(n, vector<char>(n ,'.'));
-   //0->empty cell
-   //1->queen placed
-   vector<vector<string>> ans;
-
-   int col =0; // Q1 will be placed at col 1
-
-   solve(board, 0,n,ans);
-
-   return ans;
-     }
+            board[row][col]='Q';
+            
         
-    
+        solve(board , col+1, n, ans);
+
+        board[row][col]='.';
+        }
+    }
+}
+    vector<vector<string>> solveNQueens(int n) {
+
+        vector<vector<char>> board(n, vector<char>(n, '.') );
+
+        int col=0;
+
+        vector<vector<string>> ans;
+
+        solve(board, 0, n,ans);
+
+       return ans; 
+    }
 };
