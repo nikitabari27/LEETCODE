4aa8fcf96fac0074a561b9125cd1847e89047cf2
Nikita Bari <nikitabari27@gmail.com>
2026-04-21 11:43:13 +0530

    Time: 0 ms (100%), Space: 8.5 MB (45.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
 
-int topDown(vector<int> &dp , int n){
-    // base case
+int solve(vector<int> &dp, int n){
+
+    //Base case
     if(n==0 || n==1){
         return 1;
     }
-    // step 3-> check if ans already exist
+    //check 
     if(dp[n] != -1){
         return dp[n];
     }
 
-    // step2-> store & return ans in dp
-
-    dp[n] = topDown(dp ,n-1) + topDown(dp, n-2);
+     dp[n]= solve(dp, n-1) + solve(dp , n-2);
     return dp[n];
 }
     int climbStairs(int n) {
-        // Dp top down
+        vector<int> dp(n+1 , -1);
+          
+        int ans= solve(dp , n);
 
-        //step 1 -> create dp
-
-        vector<int > dp(n+1, -1);
-
-        return topDown(dp ,n);
+        return ans;
     }
 };
