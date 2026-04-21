510ce887e6b95615e53cf45fc28f228ae3d5a0c0
Nikita Bari <nikitabari27@gmail.com>
2026-04-21 12:14:23 +0530

    Time: 16 ms (85.64%), Space: 18 MB (68.58%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,42 +3,31 @@ public:
 
 int solve(vector<int>& coins, int amount){
 
-// create dp
-        vector<int> dp(amount+1 , INT_MAX);
-    //observe base case
-    dp[0] =0;
+  vector<int>dp(amount+1 , INT_MAX);
+    dp[0] = 0;
 
-    // iterate from 0 to target amount
-    for(int target =1; target<=amount; target++){
-        // 1 se isliye start kyu ki 0 base case m already aa chuka h
+for(int target =1; target <=amount; target++){
+    int mini =INT_MAX;
+    for(int i=0; i<coins.size(); i++){
 
+        if(target-coins[i]>=0){
+            int ans=dp[target-coins[i]];
             
-            int mini = INT_MAX;
+        
 
-        for(int i=0; i<coins.size(); i++){
-            
-            
-            if(target-coins[i] >= 0){
-            int ans = dp[target - coins[i]];
-
-            if(ans != INT_MAX){
-                mini = min(mini, ans+1);
-            }
-         }
-         
-      
+        if(ans != INT_MAX){
+            mini =min(mini, 1+ans);
+          }
         }
-         dp[target] = mini;
     }
-     return dp[amount]; 
+        dp[target] = mini;
+    }
+    return dp[amount];
 }
     int coinChange(vector<int>& coins, int amount) {
-        
     
-
-        int ans= solve(coins, amount);
-
-        if(ans == INT_MAX){
+        int ans= solve(coins , amount);
+         if(ans == INT_MAX){
             return -1;
         }
         else{
