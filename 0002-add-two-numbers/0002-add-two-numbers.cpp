607811e13075885e49f959e7428edb58522dc822
Nikita Bari <nikitabari27@gmail.com>
2026-05-11 19:29:06 +0530

    Time: 0 ms (100%), Space: 77.2 MB (43.75%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,125 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+                  //ADD
+
+          ListNode* ansHead =NULL;
+          ListNode* ansTail =NULL;
+          int carry=0;
+        
+        //case 1
+        while(l1 != NULL && l2 != NULL ){
+            
+          int sum= carry + l1->val +l2->val;
+
+          int digit = sum % 10;
+           carry = sum / 10;
+
+          // create a newNode for digit
+          ListNode* newNode =new ListNode(digit);
+
+         // insert newNode
+          if(ansTail == NULL){
+               ansTail = newNode;
+               ansHead = newNode;
+          }
+          else{
+            // Mtlb node present h
+              ansTail ->next = newNode;
+             // update tail
+              ansTail = newNode;
+          }
+          l1 = l1->next;
+          l2 = l2->next;
+        }
+
+        //case 2
+        while(l1 != NULL){
+
+         int sum= carry + l1->val ;
+
+          int digit = sum % 10;
+           carry = sum / 10;
+
+          // create a newNode for digit
+          ListNode* newNode =new ListNode(digit);
+
+             // insert newNode
+          if(ansTail == NULL){
+               ansTail = newNode;
+               ansHead = newNode;
+          }
+          else{
+            // Mtlb node present h
+              ansTail ->next = newNode;
+             // update tail
+              ansTail = newNode;
+          }
+     
+
+              l1 =l1->next;
+        }
+
+        //case 3
+        while(l2 != NULL){
+
+         int sum= carry + l2->val ;
+
+          int digit = sum % 10;
+           carry = sum / 10;
+
+          // create a newNode for digit
+          ListNode* newNode =new ListNode(digit);
+
+             // insert newNode
+          if(ansTail == NULL){
+               ansTail = newNode;
+               ansHead = newNode;
+          }
+          else{
+            // Mtlb node present h
+              ansTail ->next = newNode;
+             // update tail
+              ansTail = newNode;
+          }
+              l2 =l2->next;
+         }
+
+         // case 4
+         while(carry != 0){
+ 
+          int sum= carry;
+
+          int digit = sum % 10;
+           carry = sum / 10;
+
+          // create a newNode for digit
+          ListNode* newNode =new ListNode(digit);
+
+             // insert newNode
+          if(ansTail == NULL){
+               ansTail = newNode;
+               ansHead = newNode;
+          }
+          else{
+            // Mtlb node present h
+              ansTail ->next = newNode;
+             // update tail
+              ansTail = newNode;
+          }
+         }
+         return ansHead;
+    }
+};
