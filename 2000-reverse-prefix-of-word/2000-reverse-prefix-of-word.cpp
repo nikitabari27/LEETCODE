9e32054f72f671bacd06ef3f907460ef6126192e
Nikita Bari <nikitabari27@gmail.com>
2026-06-10 13:44:40 +0530

    Time: 2 ms (4.64%), Space: 8.4 MB (65.21%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -4,18 +4,22 @@ public:
 
         stack<char>str;
         int index =-1;
-
+ 
+// sbse pehle we will find the index jha tk hme reverse krna h
         for(int i=0; i<word.size(); i++){
             if(word[i] == ch){
                 index = i;
                 break;
             }
         }
+        // fir string ko stack m push kiya
+        // Jo index value mili thi wha tk hi push kiya
         for(int i=0; i<=index; i++){
             str.push(word[i]);
         }
         string ans =""; 
 
+       // fir stack se reverse kr diya
         while(!str.empty()){
             ans += str.top();
             str.pop();
