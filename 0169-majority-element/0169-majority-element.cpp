557ada4f115d94f41828af1521e354d1e565fe5c
Nikita Bari <nikitabari27@gmail.com>
2026-07-31 20:31:10 +0530

    Time: 0 ms (100%), Space: 28.3 MB (39.03%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+
+        // Boyre Moyre voting algo
+
+        int count =0;
+        int candidate= 0;
+
+        for(int i=0; i<nums.size(); i++){
+
+            if(count==0){
+                candidate= nums[i];
+            }
+
+            if(candidate==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return candidate;
+    }
+};
