0389821363aa49d36695c7855dca55f94c2eea1d
Nikita Bari <nikitabari27@gmail.com>
2026-08-23 19:27:23 +0530

    Time: 4 ms (19.26%), Space: 28.5 MB (14.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
+
+static bool cmp(pair<int, int> a, pair<int, int>b){
+    return a.second > b.second;
+}
     int majorityElement(vector<int>& nums) {
         
-
-        // Boyre Moyre voting algo
-
-        int count =0;
-        int candidate= 0;
+        unordered_map<int, int> mp;
 
         for(int i=0; i<nums.size(); i++){
 
-            if(count==0){
-                candidate= nums[i];
-            }
-
-            if(candidate==nums[i]){
-                count++;
-            }
-            else{
-                count--;
-            }
+            mp[nums[i]]++;
         }
-        return candidate;
+
+        vector<pair <int, int>> v;
+
+        for(auto i : mp){
+            v.push_back({i.first, i.second});
+        }
+
+        sort(v.begin(), v.end(), cmp);
+       
+       return v[0].first;
     }
 };
