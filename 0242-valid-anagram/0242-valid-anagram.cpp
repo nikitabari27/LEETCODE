543092565fb3e20ab63635717e1a8b46c2dfef75
Nikita Bari <nikitabari27@gmail.com>
2026-08-23 19:06:38 +0530

    Time: 5 ms (25.37%), Space: 10.4 MB (11.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,15 +2,28 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-
-        if(s.size()!=t.size()){
+        if(s.length()!= t.length()){
             return false;
         }
-        sort(s.begin() , s.end());
-        sort(t.begin() , t.end());
-
-        return s==t;
         
-    
+     unordered_map<char, int>mp;
+
+     for(int i=0; i<s.size(); i++){
+          char ch = s[i];
+          mp[ch]++;
+     }
+
+     unordered_map<char, int>mp1;
+
+     for(int i=0; i<t.size(); i++){
+          char ch = t[i];
+          mp1[ch]++;
+     }
+    for(int i=0; i<mp.size(); i++){
+      if(mp[i]!=mp1[i])return false;
+
+     
+     }
+     return true;
     }
 };
