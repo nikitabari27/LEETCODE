4f08baef35c11be63497b458672f47e015050d3c
Nikita Bari <nikitabari27@gmail.com>
2026-08-23 19:33:51 +0530

    Time: 0 ms (100%), Space: 28 MB (99.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-
-static bool cmp(pair<int, int> a, pair<int, int>b){
-    return a.second > b.second;
-}
     int majorityElement(vector<int>& nums) {
         
-        unordered_map<int, int> mp;
+        int count=0;
+        int candidate =0;
 
         for(int i=0; i<nums.size(); i++){
 
-            mp[nums[i]]++;
+            if(count==0){
+                candidate = nums[i];
+            }
+
+            if(candidate == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
         }
-
-        vector<pair <int, int>> v;
-
-        for(auto i : mp){
-            v.push_back({i.first, i.second});
-        }
-
-        sort(v.begin(), v.end(), cmp);
-       
-       return v[0].first;
+        return candidate;
     }
 };
