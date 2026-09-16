c4199e75364fe4da1b06fa5218b1996be611468c
Nikita Bari <nikitabari27@gmail.com>
2026-09-16 12:46:53 +0530

    Time: 69 ms (25.2%), Space: 152.5 MB (12.41%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,51 +1,40 @@
 class MinStack {
 public:
-
-    vector<pair<int,int>> v;
-
-    // first int value show kr tha h
-    // second int min val show kr tha h
+  vector<pair<int, int>> v;
     MinStack() {
         
     }
     
     void push(int value) {
- 
-  // Mtlb jb apna vector already empty h tb first aur second dono hi place pr val dalege
+        
         if(v.empty()){
-            pair<int, int>p;
-            p.first =value;
-            p.second =value;
+
+            pair<int, int> p;
+
+            p.first= value;
+            p.second= value;
 
             v.push_back(p);
         }
-
-        //jb vector empty na ho tb
         else{
-            pair<int, int>p;
-             p.first= value;
-              // yha to cuurent val min hogi yha to vector m insert last value of second int
-             p.second= min(value, v.back().second);
+            pair<int, int> p;
+            p.first= value;
+            p.second= min(value, v.back().second );
 
-             v.push_back(p);
+            v.push_back(p);
         }
-        
     }
     
     void pop() {
         v.pop_back();
-        
     }
     
     int top() {
-        // vector m insert last value
        return v.back().first;
     }
     
     int getMin() {
-        
-        // min value
-        return v.back().second;
+       return v.back(). second;
     }
 };
 
